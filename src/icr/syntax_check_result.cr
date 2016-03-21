422f951f99ea4f774adcc39069432fac1e16ddff
Potapov Sergey <blake131313@gmail.com>
2016-03-22 00:40:18 +0100

    Fix syntax error

--- src/icr/syntax_check_result.cr
+++ src/icr/syntax_check_result.cr
@@ -2,7 +2,7 @@ module Icr
   class SyntaxCheckResult
     getter :status, :error_message
 
-    def initialize(@status : Symbol, @error_message = "" : String)
+    def initialize(@status : Symbol, @error_message : String = "")
     end
   end
 end

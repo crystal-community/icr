616fc837da5dccc25b17b71ed092e66acdc293c6
Jeremy Woertink <jeremywoertink@gmail.com>
2016-09-01 15:47:08 -0700

    join the requires in to a single line. Allows for multiple requires now

--- src/icr/cli.cr
+++ src/icr/cli.cr
@@ -2,7 +2,7 @@ require "option_parser"
 require "../icr"
 
 is_debug = false
-code = ""
+libs = [] of String
 
 def print_stamp
   puts "Author: #{Icr::AUTHOR}"
@@ -30,8 +30,9 @@ OptionParser.parse! do |parser|
   end
 
   parser.on("-r FILE", "--require=FILE", "auto require FILE") do |filename|
-    code = "require \"#{filename}\""
+    libs.push(%{require "#{filename}"})
   end
 end
 
+code = libs.join(";")
 Icr::Console.new(is_debug).start(code)

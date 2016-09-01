9f06978f2159ec21623fbe2cd86f5d79ac387471
Jeremy Woertink <jeremywoertink@gmail.com>
2016-09-01 14:02:47 -0700

    move specs to integration

--- spec/integration/icr_spec.cr
+++ spec/integration/icr_spec.cr
@@ -1,6 +1,36 @@
 require "../spec_helper"
 
 describe "icr command" do
+  context "passing flag arguments" do
+    it "returns nothing for invalid option -c" do
+      icr("", "-c").should eq ""
+    end
+
+    it "returns the version for option -v" do
+      icr("", "-v").should contain(Icr::VERSION)
+    end
+
+    it "returns the help menu for option -h" do
+      icr("", "-h").should contain("Usage: icr [options]")
+    end
+
+    describe "using the -r option" do
+      it "requires the colorize lib" do
+        input = <<-CODE
+          "hello".responds_to?(:colorize)
+        CODE
+        icr(input, "-r colorize").should match /true/
+      end
+
+      it "fails when colorize is not required first" do
+        input = <<-CODE
+          "hello".responds_to?(:colorize)
+        CODE
+        icr(input).should match /false/
+      end
+    end
+  end
+
   it "returns the passed value" do
     icr("13").should match /\=> 13/
     icr("\"Saluton\"").should match /\=> "Saluton"/

d87bf0874415353676b9330d75608d2065f9f318
Jeremy Woertink <jeremywoertink@gmail.com>
2016-08-31 18:57:25 -0700

    icr spec helper can take an optional flag to the icr command

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,13 +2,15 @@ require "spec"
 require "../src/icr"
 
 # Execute icr command with the giving input and return stripped output.
-def icr(input : String)
-  cmd = "#{Icr::ROOT_PATH}/bin/icr"
+# Optionally, you can pass flags to the icr command 
+def icr(input : String, arg : String? = nil)
+  cmd = ["#{Icr::ROOT_PATH}/bin/icr"]
+  cmd.push(arg) unless arg.nil?
 
   io_in = MemoryIO.new(input)
   io_out = MemoryIO.new
   io_error = MemoryIO.new
 
-  Process.run(cmd, nil, nil, false, true, io_in, io_out, io_error)
+  Process.run(cmd.join(" "), nil, nil, false, true, io_in, io_out, io_error)
   io_out.to_s.strip
 end
